52d5462c6f7f92735524e368aecea6c42f47e408
ndknitor <nguyentuanpkn@gmail.com>
2023-06-26 14:55:39 +0700

    rename project

--- android/app/src/main/java/com/myapp/MainActivity.java
+++ android/app/src/main/java/com/myapp/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Dit Me";
+    return "Kn Template";
   }
 
   /**
--- ios/KnTemplateTests/KnTemplateTests.m
+++ ios/KnTemplateTests/KnTemplateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface DitMeTests : XCTestCase
+@interface KnTemplateTests : XCTestCase
 
 @end
 
-@implementation DitMeTests
+@implementation KnTemplateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
