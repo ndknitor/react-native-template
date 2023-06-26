109d31d385063596718906588cf70a77b379ed35
ndknitor <nguyentuanpkn@gmail.com>
2023-06-26 13:22:44 +0700

    Message for interceptor

--- components/AxiosInterceptor.tsx
+++ components/AxiosInterceptor.tsx
@@ -1,14 +1,18 @@
 import axios, { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
-import { PropsWithChildren, useEffect } from 'react'
+import { PropsWithChildren, useEffect, useState } from 'react'
 import Toast from 'react-native-root-toast';
 import { API_BASE_URL, REQUEST_TIMEOUT } from "@env";
 import colors from '../utils/colors';
 import React from 'react';
+import PageLoader from './PageLoader/PageLoader';
 const appxios = axios.create({
     baseURL: API_BASE_URL,
     timeout: REQUEST_TIMEOUT | 3000,
     validateStatus: () => true
 });
+export interface InterceptorParams {
+    loadingLock?: boolean;
+}
 export function setAuthorizationBearer(jwt?: string) {
     if (jwt) {
         appxios.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
@@ -19,30 +23,48 @@ export function setAuthorizationBearer(jwt?: string) {
 }
 
 export function AxiosInterceptor({ children }: PropsWithChildren) {
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
         const beforeRequest = (config: InternalAxiosRequestConfig) => {
+            const params = config.params as InterceptorParams;
+            if (params.loadingLock) {
+                setLoading(true);
+            }
             return config;
         }
         const requestError = (error: any) => {
-            console.log(error);
             return Promise.reject(error);
         }
         const onResponse = (response: AxiosResponse<any, any>) => {
-            console.log(`Path: ${response.config.url}; Method:${response.config.method}; Body:${response.config.data}; Status: ${response.status}`);
+            console.log(`Path: ${response.config.url}; Method:${response.config.method}; Status: ${response.status};
+            Body:${response.config.data}`);
+            const params = response.config.params as InterceptorParams;
+            if (params.loadingLock) {
+                setLoading(false);
+            }
             return response;
         }
         const onResponseError = (error: AxiosError) => {
-            if (error.code == "ERR_NETWORK" || error.code == "ECONNABORTED") {
-                Toast.show('Lỗi kết nối mạng, vui lòng thử lại sau', {
-                    duration: Toast.durations.SHORT,
-                    position: Toast.positions.BOTTOM,
-                    backgroundColor: colors.error,
-                    shadow: true,
-                    animation: true,
-                    hideOnPress: true,
-                    delay: 0
-                });
+            const params = error.config?.params as InterceptorParams;
+            if (params.loadingLock) {
+                setLoading(false);
             }
+            let message = "";
+            if (error.code == "ERR_NETWORK") {
+                message = "Lỗi kết nối với máy chủ, vui lòng thử lại sau"
+            }
+            else if (error.code == "ECONNABORTED") {
+                message = "Lỗi kết nối mạng, vui lòng thử lại sau";
+            }
+            Toast.show(message, {
+                duration: Toast.durations.SHORT,
+                position: Toast.positions.BOTTOM,
+                backgroundColor: colors.error,
+                shadow: true,
+                animation: true,
+                hideOnPress: true,
+                delay: 0
+            });
             return Promise.reject(error);
         }
         appxios.interceptors.request.use(beforeRequest, requestError);
@@ -51,6 +73,7 @@ export function AxiosInterceptor({ children }: PropsWithChildren) {
     }, [])
     return (
         <>
+            <PageLoader loading={loading} />
             {children}
         </>
     )
--- pages/Index.tsx
+++ pages/Index.tsx
@@ -4,28 +4,29 @@ import { View } from 'react-native'
 import Toast from 'react-native-root-toast';
 import useRouter from '../libs/hook/useRouter';
 import colors from '../utils/colors';
-import appxios from '../components/AxiosInterceptor';
+import appxios, { InterceptorParams } from '../components/AxiosInterceptor';
+import { AxiosProxyConfig } from 'axios';
 export default function Index() {
     const { navigate } = useRouter();
     return (
         <View style={{ height: "100%", alignItems: "center", justifyContent: "center" }}>
             <Text>Hello</Text>
             <Button onPress={async () => {
-                await appxios.get("", {
-                    params: {
-                        h: "hello"
-                    }
-                });
-                // Toast.show('Lỗi kết nối mạng, vui lòng thử lại sau', {
-                //     duration: Toast.durations.LONG,
-                //     position: Toast.positions.BOTTOM,
-                //     shadow: true,
-                //     animation: true,
-                //     hideOnPress: true,
-                //     delay: 0,
-                //     backgroundColor: colors.error
+                // await appxios.get("", {
+                //     params: {
+                //         loadingLock: true
+                //     } as InterceptorParams
                 // });
-            }} title="địt mẹ mày" />
+                Toast.show('Lỗi kết nối mạng, vui lòng thử lại sau', {
+                    duration: Toast.durations.LONG,
+                    position: Toast.positions.BOTTOM,
+                    shadow: true,
+                    animation: true,
+                    hideOnPress: true,
+                    delay: 0,
+                    backgroundColor: colors.error
+                });
+            }} title="Hello" />
             <Button onPress={() => {
                 navigate('About');
             }} title="About" />
