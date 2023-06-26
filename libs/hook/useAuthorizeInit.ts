f453808d2df5775f7da47da6940418ee7d8f2512
ndknitor <nguyentuanpkn@gmail.com>
2023-06-26 15:20:19 +0700

    Remove unused import

--- libs/hook/useAuthorizeInit.ts
+++ libs/hook/useAuthorizeInit.ts
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import useEffectOnce from "./useEffectOnce";
 import { AuthorizeContext } from "../../context/AuthorizeContextProvider";
-import { sleep } from "../functions";
 
 export default function useAuthorizeInit() {
     const { setInitLoading, initLoading } = useContext(AuthorizeContext);
