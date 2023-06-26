81be366003187fc9dc6afb46c6c3a6f025ca899a
ndknitor <nguyentuanpkn@gmail.com>
2023-06-26 14:25:37 +0700

    Commit to change name

--- ios/DitMeTests/DitMeTests.m
+++ ios/DitMeTests/DitMeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TravelAppTests : XCTestCase
+@interface DitMeTests : XCTestCase
 
 @end
 
-@implementation TravelAppTests
+@implementation DitMeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
