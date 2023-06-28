954587b2a61f43bd6a19311dee576d974df679a8
ndknitor <nguyentuanpkn@gmail.com>
2023-06-28 10:23:48 +0700

    Change OneSignal init

--- App.tsx
+++ App.tsx
@@ -5,9 +5,26 @@ import { GlobalContextProvider } from './context/GlobalContextProvider';
 import AuthorizeContextProvider from './context/AuthorizeContextProvider';
 import { RootSiblingParent } from 'react-native-root-siblings';
 
+// import OneSignal from 'react-native-onesignal';
+// import { ONESIGNAL_APP_ID } from '@env';
+
+// OneSignal.setAppId(ONESIGNAL_APP_ID);
+// OneSignal.promptForPushNotificationsWithUserResponse();
+// OneSignal.setNotificationWillShowInForegroundHandler(notificationReceivedEvent => {
+//     console.log("OneSignal: notification will show in foreground:", notificationReceivedEvent);
+//     let notification = notificationReceivedEvent.getNotification();
+//     console.log("notification: ", notification);
+//     const data = notification.additionalData
+//     console.log("additionalData: ", data);
+//     notificationReceivedEvent.complete(notification);
+// });
+// OneSignal.setNotificationOpenedHandler(notification => {
+//     console.log("OneSignal: notification opened:", notification);
+// });
+
+
+
 function App(): JSX.Element {
-
-
   return (
     <RootSiblingParent>
       <AxiosInterceptor>
