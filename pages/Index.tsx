262135e1bbb818d0cdb5eeec9d1a282e53651086
ndknitor <nguyentuanpkn@gmail.com>
2023-06-29 12:30:45 +0700

    Commit

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -48,6 +48,7 @@ export default function Index() {
                         delay: 0,
                     });
                 }} >Sample toast</Button>
+            <HorizontalSpace />
             <Button
                 mode='contained'
                 onPress={() => {
