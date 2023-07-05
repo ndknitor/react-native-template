cc687db7c5b852b425de62a1a308973c3f34a3fe
kn <nguyentuanpkn@gmail.com>
2023-07-05 16:44:18 +0700

    useScreenDimensions

---
+++ libs/hook/useScreenDimensions.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import { ScaledSize, Dimensions } from 'react-native';
+
+const useScreenDimensions = (): ScaledSize => {
+    const [screenDimensions, setScreenDimensions] = useState(Dimensions.get('window'));
+
+    useEffect(() => {
+        const handleDimensionsChange = ({ window }: { window: ScaledSize }) => {
+            setScreenDimensions(window);
+        };
+        const listener = Dimensions.addEventListener('change', handleDimensionsChange);
+        return () => {
+            listener.remove();
+        }
+    }, []);
+
+    return screenDimensions;
+};
+
+export default useScreenDimensions;
