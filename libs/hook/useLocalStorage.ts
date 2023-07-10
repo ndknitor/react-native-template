272aed0dedc07dc788231c8691ccd9e279f00f67
kn <nguyentuanpkn@gmail.com>
2023-07-10 08:05:50 +0700

    useLocalStorage

---
+++ libs/hook/useLocalStorage.ts
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import useAsyncEffect from 'use-async-effect';
+
+export default function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T) => void] {
+  const [value, setValue] = useState(initialValue);
+
+  useAsyncEffect(async () => {
+    try {
+      const storedValue = await AsyncStorage.getItem(key);
+      if (storedValue !== null) {
+        setValue(JSON.parse(storedValue));
+      }
+    } catch (error) {
+      console.log('Error retrieving data from AsyncStorage:', error);
+    }
+  }, [key]);
+
+  useAsyncEffect(async () => {
+    try {
+      await AsyncStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.log('Error saving data to AsyncStorage:', error);
+    }
+  }, [key, value]);
+
+  return [value, setValue];
+}
