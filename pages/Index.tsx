0851acaa24be0fa75212f95a939ab43444161385
kn <kn@debian-BULLSEYE-live-builder-AMD64>
2023-07-31 14:25:29 +0700

    Move to about

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -1,77 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { View } from 'react-native'
-import Toast from 'react-native-root-toast';
 import useRouter from '../libs/hook/useRouter';
-import { useFormik } from 'formik';
-import appxios, { InterceptorParams } from '../components/AxiosInterceptor';
-import SignInRequest from '../objects/requests/SignInRequest';
-import { ActivityIndicator, Button, Text } from 'react-native-paper';
-import InfiniteScrollView from '../components/InfiniteScrollView/InfiniteScrollView';
-import { sleep } from '../libs/functions';
-import HorizontalSpace from '../components/HorizontalSpace/HorizontalSpace';
-import ThemeTextInput from '../components/ThemeTextInput/ThemeTextInput';
-import ScaleFadeTransition from '../components/ScaleFadeTransition/ScaleFadeTransition';
-import FadeInView from '../components/FadeInView/FadeInView';
-import ZoomInView from '../components/ZoomInView/ZoomInView';
-import Animated, { FadeIn, ZoomIn } from 'react-native-reanimated';
+import { Button } from 'react-native-paper';
 interface Item {
     id: number;
     name: string;
 }
 export default function Index() {
     const { navigate } = useRouter();
-    const formik = useFormik({
-        initialValues: SignInRequest.getDefault(),
-        validationSchema: SignInRequest,
-        onSubmit: (values) => {
-            Toast.show('Địt mẹ mày', {
-                duration: Toast.durations.LONG,
-                position: Toast.positions.BOTTOM,
-                shadow: false,
-                animation: true,
-                hideOnPress: true,
-                delay: 0,
-            });
-        },
-    });
-    const [showed, setShowed] = useState(true);
     return (
-        <Animated.View entering={ZoomIn.duration(1200)} style={{ height: "100%", alignItems: "center", justifyContent: "center", padding: 10 }}>
-            <View style={{ backgroundColor: "red", height: 90, width: 90 }}>
-            </View>
+        <View style={{ alignItems: "center", justifyContent: "center", width: "100%", height: "100%" }}>
+            <Button mode='contained' onPress={() => navigate("About")}>About</Button>
             {/* <AssetSvg.Ruby width={100} height={100} /> */}
-            <Text variant='titleLarge'>Anh có tất cả, nhưng lại thiếu em</Text>
-            <Button
-                mode='contained'
-                onPress={async () => {
-                    await appxios.get("", {
-                        loadAction: {
-                            loadingLock: true
-                        }
-                    });
-                    Toast.show("Dit me may");
-                }} >Click</Button>
-            <HorizontalSpace />
-            <Button
-                mode='contained'
-                onPress={() => {
-                    navigate('About');
-                }} >About</Button>
-
-            <View style={{ width: "100%", rowGap: 10 }}>
-                <ThemeTextInput
-                    formik={formik}
-                    name='email'
-                    label="Email"
-                />
-                <ThemeTextInput
-                    formik={formik}
-                    name='password'
-                    label="Password"
-                    secureTextEntry
-                />
-                <Button mode='contained' onPress={() => formik.handleSubmit()}>Submit</Button>
-            </View>
-        </Animated.View>
+            {/* <Text variant='titleLarge'>Anh có tất cả, nhưng lại thiếu em</Text>*/}
+        </View>
     )
 }
--- pages/public/About.tsx
+++ pages/public/About.tsx
@@ -1,11 +1,57 @@
 import React from 'react'
 import { View } from 'react-native'
-import { Text } from 'react-native-paper'
+import { Button, Text } from 'react-native-paper'
+import Animated, { FadeInDown } from 'react-native-reanimated'
+import appxios from '../../components/AxiosInterceptor'
+import Toast from 'react-native-root-toast'
+import ThemeTextInput from '../../components/ThemeTextInput/ThemeTextInput'
+import { useFormik } from 'formik'
+import SignInRequest from '../../objects/requests/SignInRequest'
 
 export default function About() {
+  const formik = useFormik({
+    initialValues: SignInRequest.getDefault(),
+    validationSchema: SignInRequest,
+    onSubmit: (values) => {
+      Toast.show('Địt mẹ mày', {
+        duration: Toast.durations.LONG,
+        position: Toast.positions.BOTTOM,
+        shadow: false,
+        animation: true,
+        hideOnPress: true,
+        delay: 0,
+      });
+    },
+  });
   return (
-    <View>
-      <Text>About</Text>
-    </View>
+    <Animated.View entering={FadeInDown.duration(600)} style={{ width: "100%", height: "100%" }}>
+      <View style={{ rowGap: 10, padding: 15, width: "100%", height: "100%", alignItems: "center", justifyContent: "center" }}>
+        <View style={{ backgroundColor: "red", height: 90, width: 90 }} />
+        <Button
+          mode='contained'
+          onPress={async () => {
+            await appxios.get("", {
+              loadAction: {
+                loadingLock: true
+              }
+            });
+            Toast.show("Dit me may");
+          }} >Click</Button>
+        <View style={{ width: "100%", rowGap: 10 }}>
+          <ThemeTextInput
+            formik={formik}
+            name='email'
+            label="Email"
+          />
+          <ThemeTextInput
+            formik={formik}
+            name='password'
+            label="Password"
+            secureTextEntry
+          />
+          <Button mode='contained' onPress={() => formik.handleSubmit()}>Submit</Button>
+        </View>
+      </View>
+    </Animated.View>
   )
 }
