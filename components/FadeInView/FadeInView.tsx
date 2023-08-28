95619a73914a709016b0f21e1b1f96fb2f23ebf8
kn <kn@debian-BULLSEYE-live-builder-AMD64>
2023-08-28 16:58:23 +0700

    RecycleScrollView

--- components/FadeInView/FadeInView.tsx
+++ components/FadeInView/FadeInView.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { randomInt } from '../../libs/functions';
-import { View } from 'react-native-reanimated/lib/typescript/Animated';
-import { AnimateProps, FadeIn } from 'react-native-reanimated';
+import Animated, { AnimateProps, FadeIn } from 'react-native-reanimated';
 import { ViewProps } from 'react-native';
 interface FadeInViewProps extends AnimateProps<ViewProps> {
     duration?: number;
 }
 function FadeInView(props: FadeInViewProps) {
     return (
-        <View {...props} entering={FadeIn.duration(randomInt(200, 1200))} >
+        <Animated.View {...props} entering={FadeIn.duration(randomInt(600, 1200))} >
             {props.children}
-        </View>
+        </Animated.View>
     )
 }
 
