3d02efeee19bf7c740820b1a150cca8a7b593585
kn <nguyentuanpkn@gmail.com>
2023-09-08 13:11:19 +0700

    success

--- objects/requests/SignInRequest.ts
+++ objects/requests/SignInRequest.ts
@@ -1,17 +1,16 @@
 import * as Yup from 'yup';
-export default function SignInRequest(lang: string = "en") {
-    return Yup.object().shape({
-        email: Yup
-            .string()
-            .email('Invalid email')
-            .required('Email is required')
-            .max(48, "Email can't longer than 48 characters")
-            .default(""),
-        password: Yup
-            .string()
-            .min(6, 'Password must be at least 6 characters')
-            .max(512, "Password can't longer than 512 characters")
-            .required('Password is required')
-            .default("")
-    });
-}
+
+export default Yup.object().shape({
+    email: Yup
+        .string()
+        .email('Invalid email')
+        .required('Email is required')
+        .max(48, "Email can't longer than 48 characters")
+        .default(""),
+    password: Yup
+        .string()
+        .min(6, 'Password must be at least 6 characters')
+        .max(512, "Password can't longer than 512 characters")
+        .required('Password is required')
+        .default("")
+});
--- objects/responses/SingleResponse.ts
+++ objects/responses/SingleResponse.ts
@@ -1,6 +1,5 @@
 import StandardReponse from "./StandardReponse";
 
 export default interface SingleResponse<T> extends StandardReponse {
-    success: boolean;
     data: T;
 }
