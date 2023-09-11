d59275775b356a02a4f9f2d292733804ed41ce39
kn <nguyentuanpkn@gmail.com>
2023-09-11 16:12:23 +0700

    Language

--- context/GlobalContextProvider.tsx
+++ context/GlobalContextProvider.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { createContext, useContext, Dispatch, SetStateAction, useState, PropsWithChildren } from "react";
+import Languages from "../utils/language";
 
 interface ContextProps {
-    userId: string,
-    setUserId: Dispatch<SetStateAction<string>>,
+    languages: typeof Languages.en;
+    setISO: Dispatch<SetStateAction<keyof Languages>>;
 }
 
 const GlobalContext = createContext<ContextProps>({
-    userId: '',
-    setUserId: (): string => '',
+    languages: Languages.en,
+    setISO: (): keyof Languages => "en"
 })
 
 export const GlobalContextProvider = ({ children }: PropsWithChildren) => {
-    const [userId, setUserId] = useState('');
+    const [iso, setISO] = useState<keyof Languages>("en");
 
     return (
-        <GlobalContext.Provider value={{ userId, setUserId }}>
+        <GlobalContext.Provider value={{ setISO, languages: Languages[iso] }}>
             {children}
         </GlobalContext.Provider>
     )
