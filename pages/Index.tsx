5cfddb77df3be2ca57948b976a51978a5b8ac036
ndkn <nguyentuanpkn@gmail.com>
2023-11-06 22:34:31 +0700

    d

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { View } from 'react-native'
 import useRouter from '../libs/hook/useRouter';
-import { Button } from 'react-native-paper';
+import { Button , Text} from 'react-native-paper';
 export default function Index() {
     const { navigate } = useRouter();
     const [search, setSearch] = useState("");
@@ -9,7 +9,7 @@ export default function Index() {
         <View style={{ alignItems: "center", justifyContent: "center", width: "100%", height: "100%" }}>
             <Button mode='contained' onPress={() => navigate("About")}>About</Button>
             {/* <AssetSvg.Ruby width={100} height={100} /> */}
-            {/* <Text variant='titleLarge'>Anh có tất cả, nhưng lại thiếu em</Text>*/}
+            <Text variant='titleLarge'>Địt mẹ thằng Duy Nứng Dái</Text>
         </View>
     )
 }
