e3c03af044b26a2d4c06ea321723f65233246a0b
kn <nguyentuanpkn@gmail.com>
2024-01-31 14:00:54 +0700

    upgrade all packages

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View } from 'react-native'
+import { TextInput, View } from 'react-native'
 import useRouter from '../utils/useRouter';
 import { Button, Text } from 'react-native-paper';
 import useGlobalCount from '../context/hooks/useGlobalCount';
@@ -8,13 +8,25 @@ import { API_BASE_URL } from '../env';
 export default function Index() {
   const { navigate } = useRouter();
   const count = useGlobalCount();
+  // const [scan, setScan] = useState("");
+
   return (
-    <View style={{ alignItems: "center", justifyContent: "center", width: "100%", height: "100%" }}>
+    <View
+      style={{ alignItems: "center", justifyContent: "center", width: "100%", height: "100%" }}>
       <Text>{API_BASE_URL}</Text>
       <Button mode='contained' onPress={count.increase}>{count.value}</Button>
       <Button mode='contained' onPress={() => navigate("About")}>About</Button>
       {/* <AssetSvg.Ruby width={100} height={100} /> */}
-      {/* <Text variant='titleLarge'>Địt mẹ thằng Duy Nứng Dái</Text> */}
+      {/* <TextInput
+        showSoftInputOnFocus={false}
+        blurOnSubmit={false}
+        autoFocus
+        onChangeText={setScan}
+        value={scan}
+        onSubmitEditing={(e) => {
+          console.log(scan);
+          setScan("");
+        }} /> */}
     </View>
   )
 }
