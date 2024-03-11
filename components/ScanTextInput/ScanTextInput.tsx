bfc725dc5296993fde0d6a15e19e699219552ffe
kn <nguyentuanpkn@gmail.com>
2024-03-11 08:30:45 +0700

    ScanTextInput

--- components/ScanTextInput/ScanTextInput.tsx
+++ components/ScanTextInput/ScanTextInput.tsx
@@ -1,24 +1,39 @@
-import React, { useState } from 'react'
+import React, { RefObject, useEffect, useRef, useState } from 'react'
 import { TextInput, TextInputProps } from 'react-native'
+import useRouter from '../../utils/useRouter';
 interface ScanTextInputProps extends TextInputProps {
     onScanSubmit: (e: string) => void | Promise<void>;
+    forceFocus?: boolean;
 }
 function ScanTextInput(props: ScanTextInputProps) {
     const [value, setValue] = useState("");
+    const navigation = useRouter();
+    const textInputRef = useRef<TextInput>(null);
+
+    useEffect(() => {
+        const listener = navigation.addListener("focus", e => {
+            setTimeout(() => textInputRef.current?.focus(), 300);
+        });
+        return () => {
+            navigation.removeListener("focus", listener);
+        }
+    }, []);
+
     return (
         <TextInput
-            value={props.value || value}
+            value={value}
             onChangeText={props.onChangeText || setValue}
             showSoftInputOnFocus={false}
             blurOnSubmit={false}
             autoFocus={props.autoFocus || true}
             style={{ width: 0, height: 0, position: "absolute", zIndex: -9999, top: -99 }}
-            onSubmitEditing={(e) => {
+            onSubmitEditing={async (e) => {
                 props.onSubmitEditing && props.onSubmitEditing(e);
-                props.onScanSubmit(value);
+                await props.onScanSubmit(value);
                 setValue("");
             }}
             {...props}
+            ref={textInputRef}
         />
     )
 }
--- pages/Index.tsx
+++ pages/Index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { TextInput, View } from 'react-native'
 import { Button, Text } from 'react-native-paper';
 import useGlobalCount from '../context/hooks/useGlobalCount';
@@ -12,6 +12,7 @@ export default function Index(props: StackScreenProps<ParamListBase>) {
   const { navigate } = props.navigation;
   const count = useGlobalCount();
   // const [scan, setScan] = useState("");
+
   return (
     <View
       style={{ alignItems: "center", justifyContent: "center", width: "100%", height: "100%" }}>
@@ -20,8 +21,6 @@ export default function Index(props: StackScreenProps<ParamListBase>) {
       <Button mode='contained' onPress={() => navigate("About")}>About</Button>
       {/* <AssetSvg.Ruby width={100} height={100} /> */}
       <ScanTextInput
-        // onChangeText={setScan}
-        // value={scan}
         onScanSubmit={Toast.show}
       />
     </View>
