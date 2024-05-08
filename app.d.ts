25ce04451a946b3b4307f8a3c8d88b45bdc77b0d
ndknitor <nguyentuanpkn@gmail.com>
2024-05-08 15:22:28 +0700

    Update all packages

---
+++ app.d.ts
@@ -0,0 +1,9 @@
+import { ImageSourcePropType } from 'react-native';
+declare module '*.png' {
+    const value: ImageSourcePropType;
+    export default value;
+}
+declare module '*.jpg' {
+    const value: ImageSourcePropType;
+    export default value;
+}
